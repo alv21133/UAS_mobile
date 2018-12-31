a70aa00c885c3ea5574ce6af9fe9aa9ed55caca3
alv21133 <wahyudi.0060@students.amikom.ac.id>
2018-12-31 18:14:06 +0700

    tambah ikon & funngsi notificaion

--- app/src/main/java/id/ac/amikom/a0060students/wahyudi/filemku/FilemDetailActivity.java
+++ app/src/main/java/id/ac/amikom/a0060students/wahyudi/filemku/FilemDetailActivity.java
@@ -1,8 +1,19 @@
 package id.ac.amikom.a0060students.wahyudi.filemku;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.Image;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +25,7 @@ public class FilemDetailActivity extends AppCompatActivity {
     private TextView txtRingkasan;
     private  TextView txttayang;
     private ImageView imgPoster;
+    private ImageButton notification;
 
 
     @Override
@@ -30,12 +42,49 @@ public class FilemDetailActivity extends AppCompatActivity {
         txtRingkasan.setText(f.getTxtRingkasan());
         txttayang=(TextView) findViewById(R.id.txtdatatayang);
         txttayang.setText(f.gettayang());
+        notification = (ImageButton) findViewById(R.id.notif);
 
         imgPoster=(ImageView) findViewById(R.id.coverFilem);
         Glide.with(getApplicationContext())
                 .load(f.getImgPoster()).override(350,350).into(imgPoster);
 
         Log.d("",f.getImgPoster());
+
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotif();
+            }
+        });
+
+
+
+    }
+    private void showNotif() {
+        NotificationManager notificationManager;
+
+        Intent mIntent = new Intent(this, FilemDetailActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("fromnotif", "notif");
+        mIntent.putExtras(bundle);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setColor(getResources().getColor(R.color.colorAccent));
+        builder.setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.favorite_i)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.favorite_i))
+                .setTicker("Filem Favorit di tambahkan ")
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setLights(Color.RED, 3000, 3000)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setContentTitle("Filemku")
+                .setContentText("Filem Favorit telah di tambahkan ");
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(115, builder.build());
+
 
     }
 }
